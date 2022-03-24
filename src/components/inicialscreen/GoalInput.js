a7043b57c61f5d69c0cc6f675e6b2c5c8a5cf8d3
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-03-24 09:54:11 -0300

    Preparando revisão de código

--- src/components/inicialscreen/GoalInput.js
+++
@@ -1,8 +0,0 @@
-export default function GoalInput(props){
-    
-    return (
-        <input className="goal-input box" 
-        placeholder="Digite nova meta se quiser..." 
-        onChange={e => e.target.value==="" ? props.setGoal(8) : props.setGoal(e.target.value)}/>
-    );
-}
